eb75e17f071196f54a032cc9cc7f0ec812dc66b5
Amogh Ratna Manandhar <manandhar.amogh@gmail.com>
2026-06-23 22:22:05 +0200

    rename changed

--- hcc-line-chart/gulpfile.js
+++ hcc-line-chart/gulpfile.js
@@ -4,6 +4,6 @@ const rename = require('gulp-rename');
 // Task to rename index.umd.js to chart.umd.js
 gulp.task('rename-umd', function () {
   return gulp.src('lib/index.umd.js') 
-    .pipe(rename('hcc_line_chart.js'))  
+    .pipe(rename('hcc-line-chart.js'))  
     .pipe(gulp.dest('lib'));          
 });
